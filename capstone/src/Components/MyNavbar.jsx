845cf4564d353a471784a26b06cff3806fc1e5d4
MaurizioCr <crispinomaurizio034@gmail.com>
2024-02-19 16:17:22 +0100

    Avatar utente

--- capstone/src/Components/MyNavbar.jsx
+++ capstone/src/Components/MyNavbar.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
@@ -6,10 +6,9 @@ import styled from "styled-components";
 import { Link, useLocation, useNavigate } from 'react-router-dom';
 import { Col, Row } from 'react-bootstrap';
 
-
 const StyledHeader = styled.div`
-.grigio{
-  background: rgba(128, 128, 128, 0.4) url(./Assets/foto1.jpg);
+  .grigio {
+    background: rgba(128, 128, 128, 0.4) url(./Assets/foto1.jpg);
   }
   .contentend {
     justify-content: end !important;
@@ -19,10 +18,9 @@ const StyledHeader = styled.div`
     font-weight: bold;
   }
 
-  .hover{
-    cursor:pointer;
+  .hover {
+    cursor: pointer;
   }
-  
 `;
 
 const LoginPrompt = styled.div`
@@ -30,16 +28,47 @@ const LoginPrompt = styled.div`
   justify-content: center;
   align-items: center;
   height: 200px;
-  background-color: rgba(255, 255, 255, 0.8); 
-  margin-top: 20px; 
-  
+  background-color: rgba(255, 255, 255, 0.8);
+  margin-top: 20px;
 `;
-
 
 const Prova = ({ children }) => {
   const location = useLocation();
   const navigate = useNavigate();
+  const [user, setUser] = useState({});
   const isAuthenticated = !!localStorage.getItem("authToken");
+
+  useEffect(() => {
+    async function fetchUserData() {
+      try {
+        const userId = localStorage.getItem("userId");
+
+        if (!userId) {
+          console.error('User ID not found in local storage.');
+          return;
+        }
+
+        const response = await fetch(`${process.env.REACT_APP_BACKEND}/users/${userId}`, {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem('authToken')}`,
+          },
+        });
+
+        if (response.ok) {
+          const userData = await response.json();
+          setUser(userData);
+        } else {
+          console.error('Error fetching user data. Server response:', response.status, response.statusText);
+        }
+      } catch (error) {
+        console.error('Error fetching user data:', error);
+      }
+    }
+
+    if (isAuthenticated) {
+      fetchUserData();
+    }
+  }, [isAuthenticated]);
 
   function logout() {
     localStorage.removeItem("authToken");
@@ -51,18 +80,25 @@ const Prova = ({ children }) => {
     <StyledHeader>
       <Navbar className="justify-content-end grigio">
         <Container className='text-right'>
-          <Link className={location.pathname === '/login' ? 'nav-link active' : 'nav-link'}
-                    to="/"><h2 >LastWorld</h2></Link>
+          <Link className={location.pathname === '/login' ? 'nav-link active' : 'nav-link'} to="/">
+            <h2 >LastWorld</h2>
+          </Link>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end contentend">
             <Nav className="ml-auto">
               {isAuthenticated ? (
                 <>
-                  <Link className='nav-link' to="/profile">Vai al tuo profilo</Link>
                   <Link onClick={logout} className="nav-link" to="/">
                     Logout
                   </Link>
                   {children}
+                  <Link className='nav-link' to="/profile">
+                    {user.avatar ? (
+                      <img src={user.avatar} alt="avatar" style={{ borderRadius: '50%', width: '2em', height: '2em' }} />
+                    ) : (
+                      'Vai al tuo profilo'
+                    )}
+                  </Link>
                 </>
               ) : (
                 <>
