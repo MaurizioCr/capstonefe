2ea09b0a07fbb538a3208d6125f6bb159b11acc5
MaurizioCr <crispinomaurizio034@gmail.com>
2024-02-28 17:20:57 +0100

    Immagini laterali storia + modifiche iniziali social

--- capstone/src/Components/Social.jsx
+++ capstone/src/Components/Social.jsx
@@ -2,37 +2,47 @@
 import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
-import { Youtube, Instagram, Twitter, Discord } from 'react-bootstrap-icons';
+import { Youtube, Instagram, Twitter, Discord, Facebook, Tiktok } from 'react-bootstrap-icons';
 
 
 
 const Social = () => {
   return (
-    <Container>
-      <h1 className='text-light'>Social</h1>
-      <Row>
-        <Col>
-          <Link to="/social/facebook" target="_blank" rel="noopener noreferrer">
+    <Container className='text-center' >
+      <h1 className='text-light'>I nostri Social</h1>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/dQw4w9WgXcQ?si=ZoczmdsYCMaaMogq" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+      <Row className='mt-5 pt-5'>
+        <Col className='col-2'>
+          <a target="_blank" href='https://twitter.com/?lang=it'>
+          
             <Twitter size={50} />
-          </Link>
+          </a>
         </Col>
-        <Col>
-          <Link to="/social/twitter" target="_blank" rel="noopener noreferrer">
+        <Col className='col-2'>
+          <a target="_blank" href='https://twitter.com/?lang=it'>
             <Instagram size={50} />
-          </Link>
+          </a>
         </Col>
-        <Col>
-          <Link to="/social/instagram" target="_blank" rel="noopener noreferrer">
+        <Col className='col-2'>
+          <a target="_blank" href='https://twitter.com/?lang=it'> 
             <Youtube size={50} />
-          </Link>
-          <iframe width="560" height="315" src="https://www.youtube.com/embed/dQw4w9WgXcQ?si=ZoczmdsYCMaaMogq" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+          </a>
         </Col>
-        <Col>
-          <Link to="/social/twitter" target="_blank" rel="noopener noreferrer">
+        <Col className='col-2'>
+          <a target="_blank" href='https://twitter.com/?lang=it'>
             <Discord size={50} />
-          </Link>
+          </a>
         </Col>
-        {/* Aggiungi altri social con i relativi link */}
+        <Col className='col-2'>
+          <a target="_blank" href='https://twitter.com/?lang=it'>
+            <Facebook size={50} />
+          </a>
+        </Col>
+        <Col className='col-2'>
+          <a target="_blank" href='https://twitter.com/?lang=it'>
+            <Tiktok size={50} />
+          </a>
+        </Col>
       </Row>
     </Container>
   );
--- capstone/src/Components/Storia.jsx
+++ capstone/src/Components/Storia.jsx
@@ -3,9 +3,10 @@ import React, { useState, useEffect } from 'react';
 import { useSpring, animated } from 'react-spring';
 import { Link } from 'react-router-dom';
 import { Element, scroller } from 'react-scroll';
-import gif from "./Assets/simpson-homer.gif";
+import head from "./Assets/Scena-5_Arrabb.png";
 import { Col, Container, Row } from 'react-bootstrap';
 import 'animate.css';  // Import animate.css
+import foto1 from "./Assets/Screen-1.png";
 
 const Storia = () => {
   const [showText, setShowText] = useState(false);
@@ -32,15 +33,21 @@ const Storia = () => {
   };
 
   return (
-    <div>
-      {/* Gif continua */}
-      <Container>
-        <Row>
-          <Col className='text-center'>
-              <img className='text-center animate__animated animate__flipInX' src={gif} alt="GIF" />
-          </Col>
-        </Row>
-          <Col>
+      <div className='100-wh'>
+      <Row>
+      <Col  className='col-3 d-none d-md-flex justify-content-center  align-items-center'>
+      <img className='animate__animated animate__slideInLeft' src={foto1} width={100 + "px"} height={200 + "px"}/>
+      </Col>
+      
+      
+      
+        
+          <Col className=' col-12 col-md-6'>
+          <div className='text-center'>
+              <img style={{border: "3px solid black"}} width={400} className='rounded text-center animate__animated animate__flipInX' src={head} alt="foto Personaggio" />
+              </div>
+        
+          
             <p className='animate__animated animate__fadeInRight text-white bg-secondary p-2 m-3 rounded'>
                 Lorem Ipsum è un testo segnaposto utilizzato nel settore della tipografia e della stampa. Lorem Ipsum è considerato il testo segnaposto standard sin dal sedicesimo secolo, quando un anonimo tipografo prese una cassetta di caratteri e li assemblò per preparare un testo campione. È sopravvissuto non solo a più di cinque secoli, ma anche al passaggio alla videoimpaginazione, pervenendoci sostanzialmente inalterato. Fu reso popolare, negli anni ’60, con la diffusione dei fogli di caratteri trasferibili “Letraset”, che contenevano passaggi del Lorem Ipsum, e più recentemente da software di impaginazione come Aldus PageMaker, che includeva versioni del Lorem Ipsum.
 
@@ -53,9 +60,15 @@ const Storia = () => {
                 Da dove viene?
                 Al contrario di quanto si pensi, Lorem Ipsum non è semplicemente una sequenza casuale di caratteri. Risale ad un classico della letteratura latina del 45 AC, cosa che lo rende vecchio di 2000 anni. Richard McClintock, professore di latino al Hampden-Sydney College in Virginia, ha ricercato una delle più oscure parole latine, consectetur, da un passaggio del Lorem Ipsum e ha scoperto tra i vari testi in cui è citata, la fonte da cui è tratto il testo, le sezioni 1.10.32 and 1.10.33 del "de Finibus Bonorum et Malorum" di Cicerone. Questo testo è un trattato su teorie di etica, molto popolare nel Rinascimento. La prima riga del Lorem Ipsum, "Lorem ipsum dolor sit amet..", è tratta da un passaggio della sezione 1.10.32.
             </p>
-        </Col>
-      </Container>
-    </div>
+            </Col>
+
+            <Col className='col-3 d-none d-md-flex justify-content-center align-items-center '>
+      <img className='animate__animated animate__slideInRight' src={foto1} width={100 + "px"} height={200 + "px"}/>
+      </Col>
+        
+            </Row>
+      </div>
+    
   );
 };
 
