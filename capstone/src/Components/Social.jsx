fb8a37385a96030c40ac2ba5edc3b7cd6aca6742
MaurizioCr <crispinomaurizio034@gmail.com>
2024-03-04 16:19:57 +0100

    animazioni + struttura sezione storia +link social

--- capstone/src/Components/Social.jsx
+++ capstone/src/Components/Social.jsx
@@ -10,27 +10,36 @@ import { Youtube, Instagram, Twitter, Discord, Facebook, Tiktok } from 'react-bo
 
 const Social = () => {
   return (
-    <Container className='text-center blu' >
+    <Container className='text-center blu'>
       <Row>
         <Col>
           <h1 className='blu mb-5'>Seguici anche sui nostri Social</h1>
-          <iframe width="560" height="315" src="https://www.youtube.com/embed/dQw4w9WgXcQ?si=ZoczmdsYCMaaMogq" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+          <div>
+            <iframe
+              className='video-frame'
+              width="560" height="315"
+              src="https://www.youtube.com/embed/9q5YV-6ZXYQ?si=NITIu7J5pDGOSn3H"
+              title="YouTube video player"
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              allowFullScreen
+            ></iframe>
+          </div>
         </Col>
       </Row>
-        
-        <Col>
-          <Row className='absolute rounded bg-success w-50 py-2'>
+      <Col className=''>
+        <Row className='absolute rounded bg-success w-50 py-2'>
             <a className='blu' target="_blank" href='https://twitter.com/?lang=it'>
               <Twitter size={50} />
             </a>
           
           
-            <a className='blu' target="_blank" href='https://twitter.com/?lang=it'>
+            <a className='blu' target="_blank" href='https://www.instagram.com/lupiostudios?igsh=bGt6dHhsNnkxb3g3'>
               <Instagram size={50} />
             </a>
           
           
-            <a className='blu' target="_blank" href='https://twitter.com/?lang=it'> 
+            <a className='blu' target="_blank" href='https://www.youtube.com/@LupioStudios'> 
               <Youtube size={50} />
             </a>
           
@@ -40,12 +49,12 @@ const Social = () => {
             </a>
           
           
-            <a className='blu' target="_blank" href='https://twitter.com/?lang=it'>
+            <a className='blu' target="_blank" href='https://www.facebook.com/profile.php?id=61556916147532&mibextid=ZbWKwL'>
               <Facebook size={50} />
             </a>
           
           
-            <a className='blu' target="_blank" href='https://twitter.com/?lang=it'>
+            <a className='blu' target="_blank" href='https://vm.tiktok.com/ZGeDudGPJ/'>
               <Tiktok size={50} />
             </a>
           
--- capstone/src/Components/Storia.jsx
+++ capstone/src/Components/Storia.jsx
@@ -8,6 +8,16 @@ import { Col, Container, Row } from 'react-bootstrap';
 import 'animate.css';  // Import animate.css
 import foto1 from "./Assets/zyro-image_4.png";
 import foto2 from "./Assets/LUPIO_NOSFONDO.png";
+import sfondo from "./Assets/Scena-7.png";
+import styled from 'styled-components';
+
+const Sfondo = styled.div`
+  background-image: url(${sfondo});
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  min-height: 100vh;
+`;
 
 const Storia = () => {
   const [showText, setShowText] = useState(false);
@@ -34,37 +44,31 @@ const Storia = () => {
   };
 
   return (
-      <div className='100-wh'>
+    <Sfondo>
+      <div className='sfondostoria'>
       <Row>
       <Col  className='col-3 d-none d-md-flex justify-content-center  align-items-center'>
       <img className='animate__animated animate__slideInLeft' src={foto1} width={300 + "px"} />
       </Col>
-      
-      
-      
-        
           <Col className=' col-12 col-md-6'>
           <div className='text-center'>
-              <img style={{border: "3px solid black"}} width={400} className='rounded text-center animate__animated animate__flipInX' src={head} alt="foto Personaggio" />
+              <img style={{border: "3px solid black"}} width={350} className='rounded text-center animate__animated animate__flipInX' src={head} alt="foto Personaggio" />
               </div>
         
           
-            <p className='animate__animated animate__fadeInRight text-white bg-secondary p-2 m-3 rounded'>
-                Lorem Ipsum è un testo segnaposto utilizzato nel settore della tipografia e della stampa. Lorem Ipsum è considerato il testo segnaposto standard sin dal sedicesimo secolo, quando un anonimo tipografo prese una cassetta di caratteri e li assemblò per preparare un testo campione. È sopravvissuto non solo a più di cinque secoli, ma anche al passaggio alla videoimpaginazione, pervenendoci sostanzialmente inalterato. Fu reso popolare, negli anni ’60, con la diffusione dei fogli di caratteri trasferibili “Letraset”, che contenevano passaggi del Lorem Ipsum, e più recentemente da software di impaginazione come Aldus PageMaker, che includeva versioni del Lorem Ipsum.
-
-                Perchè lo utilizziamo?
+            <p className='animate__animated animate__fadeInRight text-white rosso p-2 m-3 rounded'>
+            Preparati ad abbandonare la realtà, immergiti nell' avventura. Scopri il nostro primo progetto: The Last Worlds,
+             una storia piena di sorprese e misteri. In questo strano pianeta troverai luoghi fantastici ancora inesplorati, 
+             popolati da bizzarre creature che ti aspettano, alcune ti accoglieranno e aiuteranno nelle tue imprese, altre saranno 
+             ostili e cercheranno vendetta. Scopri dove sono finiti tutti gli esseri umani, La tua presenza non è casuale: solo tu puoi 
+             ristabilire l'equilibrio di una terra divisa tra bene e male. I tuoi poteri e le tue abilita ti permetteranno di vivere un esperienza surreale, 
+             l'unico in grado di risollevare un pianeta in rovina sei tu. Pronto a cambiare il tuo destino e le sorti di nuovo mondo per sempre?
             </p>
 
             <div className='text-center'>
-              <img style={{border: "3px solid black"}} width={400} className='rounded text-center animate__animated animate__flipInX' src={head} alt="foto Personaggio" />
+              <img style={{border: "3px solid black"}} width={350} className='animate__animated animate__bounceInUp rounded text-center' src={sfondo} alt="foto Personaggio" />
               </div>
-            <p className='animate__animated animate__fadeInLeft text-white bg-secondary p-2 m-3 rounded'>
-                È universalmente riconosciuto che un lettore che osserva il layout di una pagina viene distratto dal contenuto testuale se questo è leggibile. Lo scopo dell’utilizzo del Lorem Ipsum è che offre una normale distribuzione delle lettere (al contrario di quanto avviene se si utilizzano brevi frasi ripetute, ad esempio “testo qui”), apparendo come un normale blocco di testo leggibile. Molti software di impaginazione e di web design utilizzano Lorem Ipsum come testo modello. Molte versioni del testo sono state prodotte negli anni, a volte casualmente, a volte di proposito (ad esempio inserendo passaggi ironici).
-
-
-                Da dove viene?
-                Al contrario di quanto si pensi, Lorem Ipsum non è semplicemente una sequenza casuale di caratteri. Risale ad un classico della letteratura latina del 45 AC, cosa che lo rende vecchio di 2000 anni. Richard McClintock, professore di latino al Hampden-Sydney College in Virginia, ha ricercato una delle più oscure parole latine, consectetur, da un passaggio del Lorem Ipsum e ha scoperto tra i vari testi in cui è citata, la fonte da cui è tratto il testo, le sezioni 1.10.32 and 1.10.33 del "de Finibus Bonorum et Malorum" di Cicerone. Questo testo è un trattato su teorie di etica, molto popolare nel Rinascimento. La prima riga del Lorem Ipsum, "Lorem ipsum dolor sit amet..", è tratta da un passaggio della sezione 1.10.32.
-            </p>
+            
             </Col>
 
             <Col className='col-3 d-none d-md-flex justify-content-center align-items-center '>
@@ -73,7 +77,7 @@ const Storia = () => {
         
             </Row>
       </div>
-    
+      </Sfondo>
   );
 };
 
