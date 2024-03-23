55f175c88ad269a62e08d15bcc2383b99d99c490
MaurizioCr <crispinomaurizio034@gmail.com>
2024-03-23 16:34:16 +0100

    Option

--- capstone/src/Components/Stripe/StripeOption.jsx
+++ capstone/src/Components/Stripe/StripeOption.jsx
@@ -5,37 +5,32 @@ const stripePromise = loadStripe(
   "pk_test_51OmYgeGUnxyubwNLD3ZzraiAkOlMyX0KtaMxsfg2PQ3cUKAtag5cdD8g0fJ8AFm7uARtonQnm1Ii2ciwpwF8XWou00ieN94tsk"
 );
 
-const StripeOption = ({ order }) => {
+const StripeOption = ({ product }) => {
   const handleClick = async () => {
     try {
       const stripe = await stripePromise;
 
       const { error } = await stripe.redirectToCheckout({
-        lineItems: order.detailsProduct.map((detail) => ({
-          price: detail.priceId,
-          quantity: 1,
-        })),
+        lineItems: [{ price: product.price, quantity: 1 }], // Converti il prezzo in centesimi
         mode: "payment",
-        successUrl: "http://localhost:3000/successfulPayment",
-        cancelUrl: "http://localhost:3000/DeniedPayment",
+        successUrl: "http://localhost:3000/StripeSuccess",
+        
       });
 
       if (error) {
-        console.error(
-          "Si è verificato un errore durante il checkout:",
-          error.message
-        );
+        console.error("Si è verificato un errore durante il checkout:", error.message);
       }
     } catch (error) {
       console.error("Si è verificato un errore:", error.message);
     }
   };
+
   return (
     <button
       className="ms-3 px-4 py-1 pb-1 rounded fw-bold text-black "
       onClick={handleClick}
     >
-      Checkout
+      Acquista
     </button>
   );
 };
